5fa340b813198ebd44a20c5b2657c09e283d96b6
Albrecht Nitsche <nitsche@einspunktnull.com>
2017-11-30 21:43:32 +0100

    huihuihui

--- src/lib/simple-serial-protocol.ts
+++ src/lib/simple-serial-protocol.ts
@@ -15,7 +15,7 @@ export class SimpleSerialProtocol {
         this._serialPort = new SerialPort(this._portname, {baudRate: baudrate, autoOpen: false});
     }
 
-    public start = (): Promise<any> => {
+    public start(): Promise<any> {
         return new Promise((resolve, reject) => {
             if (this.isRunning) {
                 reject(Error('SerialPort already connected'));
@@ -26,11 +26,13 @@ export class SimpleSerialProtocol {
                     reject(err);
                     return;
                 }
-                //Some devices, like the Arduino, reset when you open a connection to them.
+                // TODO: implement following "ready pattern" on Arduino side
+                // Quote:
+                // Some devices, like the Arduino, reset when you open a connection to them.
                 // In such cases, immediately writing to the device will cause lost data as they wont be ready to receive the data.
                 // This is often worked around by having the Arduino send a "ready" byte that your Node program waits for before writing.
-                // You can also often get away with waiting around 400ms.
-                setTimeout(resolve, 1000);
+                // You can also often get away with waiting around 400ms
+                setTimeout(resolve, 400);
             });
             this._serialPort.on('data', this.onData);
         });
@@ -40,22 +42,22 @@ export class SimpleSerialProtocol {
         return this._serialPort.isOpen;
     }
 
-    public send = (msg: string): SimpleSerialProtocol => {
+    public send(msg: string): SimpleSerialProtocol {
         this._serialPort.write(msg, 'ascii');
         return this;
     }
 
-    public registerCommand = (char: string, callback: Function): SimpleSerialProtocol => {
+    public registerCommand(char: string, callback: Function): SimpleSerialProtocol {
         this._registeredCommandCallbacks.setValue(char, callback);
         return this;
     }
 
-    public unregisterCommand = (char: string): SimpleSerialProtocol => {
+    public unregisterCommand(char: string): SimpleSerialProtocol {
         this._registeredCommandCallbacks.remove(char);
         return this;
     }
 
-    private onData = (buf: Buffer): void => {
+    private onData(buf: Buffer): void {
         let msg: string = buf.toString('ascii');
         console.log('onData:', msg);
         let len = msg.length;
