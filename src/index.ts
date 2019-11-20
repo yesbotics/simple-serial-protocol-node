542d7fa3bd0ae65dee20efdbad64aa9eec051875
Jens Kabisch (Notebook) <jens.kabisch@robole.de>
2019-11-20 17:14:37 +0100

    updated index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-// export {SimpleSerialProtocolError} from './lib/simple-serial-protocol-error';
+export {SimpleSerialProtocol} from './lib/SimpleSerialProtocol';
+export {SimpleSerialProtocolError} from './lib/SimpleSerialProtocolError';
+export {ParamsParser} from "./lib/ParamsParser";
+export {RegisteredCommand} from "./lib/RegisteredCommand";
+
+export {ParamTypeChar} from "./lib/types/ParamTypeChar";
+export {ParamTypeInt} from "./lib/types/ParamTypeInt";
+export {ParamTypeLong} from "./lib/types/ParamTypeLong";
+export {ParamTypeUnsignedShort} from "./lib/types/ParamTypeUnsignedShort";
+export {ParamTypeUnsignedLong} from "./lib/types/ParamTypeUnsignedLong";
+export {ParamTypeUnsignedInt} from "./lib/types/ParamTypeUnsignedInt";
+export {ParamTypeCharArray} from "./lib/types/ParamTypeCharArray";
+export {ParamTypeFloat} from "./lib/types/ParamTypeFloat";
+export {ParamTypeShort} from "./lib/types/ParamTypeShort";
