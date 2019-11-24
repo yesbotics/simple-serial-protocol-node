163b9d733eca0a7bb492f07b88864fc5578b88ce
Albrecht Nitsche <nitsche@einspunktnull.com>
2019-11-24 17:38:28 +0100

    echo-example is now running

--- examples/echo-example/echo-example.ts
+++ examples/echo-example/echo-example.ts
@@ -1,19 +1,25 @@
-import {SimpleSerialProtocol, ParamTypeCharArray, ParamTypeFloat} from '@yesbotics/simple-serial-protocol-node';
+import {SimpleSerialProtocol, ParamTypeCharArray, ParamTypeFloat, Baudrate} from '@yesbotics/simple-serial-protocol-node';
 
-// create instance and defining comport identifier and baudrate
-const arduino: SimpleSerialProtocol = new SimpleSerialProtocol('COM4', 9600);
+//defining serial port identifier and baudrate
+const portname: string = 'COM4';
+const bautrate: Baudrate = 9600;
+
+// create instance
+const arduino: SimpleSerialProtocol = new SimpleSerialProtocol(portname, bautrate);
 
 // define command id, callback function and expected dataytpes
 arduino.registerCommand('r', (someString: string, someFloatingPointValue: number) => {
-    console.log('received values from arduino:');
+    console.log('Received values from Arduino:');
     console.log('someString', someString);
     console.log('someFloatingPointValue', someFloatingPointValue);
 }, [ParamTypeCharArray.NAME, ParamTypeFloat.NAME]);
 
 // establish connection to arduino and wait two seconds
 // (arduino needs time to start after connect)
-arduino.init(2000).catch(console.error).then(() => {
-    console.log('arduino connected. Sending two values to arduino');
+arduino.init(2000).catch((err) => {
+    console.error('could not minit connection', err)
+}).then(() => {
+    console.log('Arduino connected. Sending two values to Arduino');
     arduino.writeCommand('s', [
         {type: ParamTypeCharArray.NAME, value: 'hey i\'m a text'},
         {type: ParamTypeFloat.NAME, value: 3.14}
