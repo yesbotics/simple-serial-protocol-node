5aaad638c7707b73f97e61a8d1d27004cb4557f9
Albrecht Nitsche <nitsche@einspunktnull.com>
2019-11-25 21:24:12 +0100

    echo-example fixed

--- examples/echo-example/echo-example.ts
+++ examples/echo-example/echo-example.ts
@@ -1,6 +1,6 @@
 import {SimpleSerialProtocol, ParamTypeCharArray, ParamTypeFloat, Baudrate} from '@yesbotics/simple-serial-protocol-node';
 
-//defining serial port identifier and baudrate
+// define serial port identifier and baudrate
 const portname: string = 'COM4';
 const bautrate: Baudrate = 9600;
 
@@ -8,22 +8,22 @@ const bautrate: Baudrate = 9600;
 const arduino: SimpleSerialProtocol = new SimpleSerialProtocol(portname, bautrate);
 
 // define command id, callback function and expected dataytpes
-arduino.registerCommand('r', (someString: string, someFloatingPointValue: number) => {
+arduino.registerCommand('s', (someString: string, someFloatingPointValue: number) => {
     console.log('Received values from Arduino:');
     console.log('someString', someString);
     console.log('someFloatingPointValue', someFloatingPointValue);
 }, [ParamTypeCharArray.NAME, ParamTypeFloat.NAME]);
 
-// establish connection to arduino and wait 2 seconds
+// establish connection to arduino and wait 3 seconds
 // give arduino time to start after getting connected (and resetted too)
-arduino.init(2000).catch((err) => {
+arduino.init(3000).catch((err) => {
     console.error('Could not init connection. reason:', err);
 }).then(() => {
     console.log('Arduino connected.');
     console.log('Now sending 2 values to Arduino');
-    arduino.writeCommand('s', [
+    arduino.writeCommand('r', [
         // in this example text should not be longer than 50 chars (max length is defined in Arduiono sketch)
-        {type: ParamTypeCharArray.NAME, value: 'hey i\'m a text'},
+        {type: ParamTypeCharArray.NAME, value: 'hey i am a text'},
         {type: ParamTypeFloat.NAME, value: 3.14159265359}
     ]);
 });
